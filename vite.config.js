852508aed9e2c254acf8bc92522a7717e71d855f
imreegall <imreegall@gmail.com>
2024-01-13 23:59:27 +0300

    Добавил сброс стилей, миксины стилей. Обновил файл конфига сборщика Vite.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `
+              @import "./src/assets/styles/_mixins.sass"
+              @import "./src/assets/styles/_variables.sass"
+            `
+      }
+    }
+  }
 })
